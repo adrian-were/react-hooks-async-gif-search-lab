d72f5aa8c25f8c9ab07c8bd46a2648d854ec36f4
Adrian <adrianshisia@gmail.com>
2023-08-04 16:44:15 +0300

    Completed assignment

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,6 @@
+import React,{useState, useEffect} from 'react'
+
+const url = "https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=YOUR API KEY&rating=g"
+function GifListContainer() {
+    return
+}
